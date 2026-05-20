dc225260921e1fc05382d8b114fde522afcac428
Othavio-Pedretti <210075@upf.br>
2026-05-19 21:47:25 -0300

    Aulagrees

--- EDD-II/Aula7/graph.cpp
+++ EDD-II/Aula7/graph.cpp
@@ -15,32 +15,69 @@ namespace graph {
             struct node
             {
                 std::string value; // valor
-                std::vector<node*> link; // lista de ponteiros para os nós de ligações
+                std::vector<node*> links; // lista de ponteiros para os nós de ligações
             };
             // tabela hash que mapea o valor do nodo para o ponteiro do nodo
             std::unordered_map<std::string, node*> nodes;
             public:
             // função para inserir um nodo
             void insert_node(const std::string& s){
-                node aux;
-                aux.value = s;
+                node* aux = new node();
+                aux->value = s;
                 nodes[s] = aux;
             }
+
             size_t size(){
                 return nodes.size();
             }
 
-            node* find_node(const std::string& s){
+            node* find(const std::string& s){
                 auto it = nodes.find(s);
-                return it == nodes.end() ? nullptr : &it->second;
+                return it == nodes.end() ? nullptr : it->second;
             }
 
             bool insert_link( const std::string& from, const std::string& to){
-                node* pfrom = find_node(from);
-                node* pto = find_node(to);
+                node* pfrom = find(from);
+                node* pto = find(to);
                 if(pfrom == nullptr || pto == nullptr) return false;
-                pfrom->link.push_back(pto);
+                pfrom->links.push_back(pto);
                 return true;
+            }
+            void export2dot(const std::string &filename){
+                std::ofstream dot(filename);
+                dot << "digraph {\n";
+                for(auto nd : nodes){ // percorre todos os nodos
+                    dot << "\t" << nd.first; // escreve o rotulo do nodo a chave
+                    if(nd.second->links.size() > 0){
+                        dot << " -> {";
+                        for(auto vizinho : nd.second->links){
+                            dot << vizinho->value;
+                        }
+                        dot << "} [color=blue]"; // fecha chaves dos vizinhos
+                    }
+                    dot << ";\n";
+                }
+                dot << "}\n"; // fecha chaves do diagraph
+            }
+            void draw(){
+                export2dot("graphED02.dot");
+                std::system("dot -Tx11 graphED02.dot");
+            }
+            size_t outdegree(const std::string s){
+                node p = find(s);
+                if (p != nullptr)return p->second->links.size();
+            }
+            size_t indegree(const std::string& s){
+                size_t contador;
+                for(auto aresta : nodes){
+                    for(auto nodo : aresta.second->links){
+                        if(nodo->value == s)contador++;
+                    }
+                }
+                return contador;
+            }
+            size_t degree(const std::string& s){
+                return indegree(s) + outdegree(s);
             }
     };
 }
--- EDD-II/Aula7/prog_teste.cpp
+++ EDD-II/Aula7/prog_teste.cpp
@@ -13,9 +13,25 @@ int main() {
     g.insert_node("F");
     g.insert_node("G");
 
-    if(g.find_node("A") != nullptr){
+    if(g.find("A") != nullptr){
         cout << "Nodo A encontrado!" << endl;
     } else {
         cout << "Nodo A nao encontrado!" << endl;
     }
+
+    g.insert_link("A","B");
+    g.insert_link("A","D");
+    g.insert_link("C","D");
+    g.insert_link("C","A");
+    g.insert_link("E","C");
+    g.insert_link("F","F");
+
+    g.draw();
+
+    cout << g.outdegree("C") << endl;
+    cout << g.indegree("C") << endl;
+    cout << g.degree("C") << endl;
+
+
+    return 0;
 }
